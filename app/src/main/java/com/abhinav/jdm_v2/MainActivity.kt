3f9e244896d6cc01bc69d0c7d8acee153b4f61d8
Abhinav Singhal <abhinavsinghal876@gmail.com>
2024-03-15 09:57:25 +0530

    Added hamburger icon and made it functional.

--- app/src/main/java/com/abhinav/jdm_v2/MainActivity.kt
+++ app/src/main/java/com/abhinav/jdm_v2/MainActivity.kt
@@ -1,21 +1,79 @@
 package com.abhinav.jdm_v2
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.MenuItem
+import android.widget.FrameLayout
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var coordinatorLayout: CoordinatorLayout
+    private lateinit var frameLayout: FrameLayout
+    private lateinit var toolbar: Toolbar
+    private lateinit var navigationLayout: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        drawerLayout = findViewById(R.id.drawer)
+        coordinatorLayout = findViewById(R.id.coordinator)
+        frameLayout = findViewById(R.id.frame)
+        toolbar = findViewById(R.id.toolbar)
+        navigationLayout = findViewById(R.id.navigation)
+
+        setUpToolbar()
+
+        val actionBarDrawerLayout =
+            ActionBarDrawerToggle(this@MainActivity, drawerLayout, R.string.open, R.string.close)
+
+        drawerLayout.addDrawerListener(actionBarDrawerLayout)
+        actionBarDrawerLayout.syncState()
+
+        navigationLayout.setNavigationItemSelectedListener {
+
+            when(it.itemId){
+                R.id.dashboard -> {
+                    Toast.makeText(this@MainActivity, "Dashboard", Toast.LENGTH_SHORT).show()
+                }
+                R.id.favourites -> {
+                    Toast.makeText(this@MainActivity, "Favourites", Toast.LENGTH_SHORT).show()
+                }
+                R.id.profile -> {
+                    Toast.makeText(this@MainActivity, "Profile", Toast.LENGTH_SHORT).show()
+                }
+                R.id.about_us -> {
+                    Toast.makeText(this@MainActivity, "About Us", Toast.LENGTH_SHORT).show()
+                }
+            }
+            return@setNavigationItemSelectedListener true
         }
-        title="JDM_V2"
+
     }
+
+    private fun setUpToolbar() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "JDM V2"
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        val id=item.itemId
+        if(id == android.R.id.home){
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
